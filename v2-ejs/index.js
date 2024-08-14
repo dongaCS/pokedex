3aec13cc863241db055d931719ed5613c1ba9dce
dongaCS <109496734+dongaCS@users.noreply.github.com>
2024-08-14 12:48:30 -0400

    fixed folder path

--- v2-ejs/index.js
+++ v2-ejs/index.js
@@ -9,7 +9,7 @@ app.set(`view engine`, `ejs`);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-const pokemonRouter = require(`./routes/pokemon/pokemon.js`)
+const pokemonRouter = require(`./routes/pokemon.js`)
 app.use(`/api/pokemon`, pokemonRouter); 
 
 app.get(`/`, (req, res) => res.send(`http://localhost:3000/api/pokemon/`))
